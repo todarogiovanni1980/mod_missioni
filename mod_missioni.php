d79ae3b78b33d36b33921058d03bffe1cc5ef69b
Giovanni Todaro <todarogiovanni1980@users.noreply.github.com>
2016-09-21 15:09:27 +0200

    parameter module's configuration
    get a parameter from the module's configuration

--- mod_missioni.php
+++ mod_missioni.php
@@ -1,7 +1,11 @@
 <?php
 //don't allow other scripts to grab and execute our file
 defined('_JEXEC') or die('Direct Access to this location is not allowed.');
+
+// get a parameter from the module's configuration
+$missionsCount = $params->get('missionscount');
 ?>
+
 <p>
-    Visualizza le Ultime Missioni secondo le impostazioni del modulo.
+    Visualizza le Ultime <?php echo $missionsCount; ?>Missioni secondo le impostazioni del modulo.
 </p>
