180eed18c6be974ea7763b3954b7233b34f867e1
Giovanni Todaro <todarogiovanni1980@users.noreply.github.com>
2016-09-21 21:30:04 +0200

    Update default.php
    aggiunto icona sullo stato del rimborso

--- tmpl/default.php
+++ tmpl/default.php
@@ -3,16 +3,20 @@
 <ul>
     <?php foreach ($items as $item) { ?>
     <li>
+    	<img
+			src="administrator/images/<?php echo ( $item->rimborso ) ? 'tick.png' : ( $item->rimborso != 1 ? 'publish_x.png' : 'disabled.png' );?>"
+			width="16" height="16" border="0"
+			alt="<?php echo ( $item->rimborso ) ? JText::_( "Rimborsata " . $item->rimborso ) : JText::_( 'Non lavorata' );?>" /> 
         <img
 			src="administrator/images/<?php echo ( $item->protocollo ) ? 'tick.png' : ( $item->protocollo != 1 ? 'publish_x.png' : 'disabled.png' );?>"
 			width="16" height="16" border="0"
 			alt="<?php echo ( $item->protocollo ) ? JText::_( "Protocollo numero " . $item->protocollo ) : JText::_( 'Non Protocollata' );?>" /> 
-		<img
+	<img
 			src="administrator/images/<?php echo ( $item->filemissionefirmata ) ? 'edit_f2.png' : ( $item->filemissionefirmata != 1 ? 'edit.png' : 'edit.png' );?>"
 			width="16" height="16" border="0"
 			alt="<?php echo ( $item->protocollo ) ? JText::_( "Firmata " . $item->protocollo ) : JText::_( 'da firmare' );?>" />  
 			
-		<?php echo $item->numero ." del ". $item->data ; ?>
+	<?php echo $item->numero ." del ". $item->data ; ?>
     </li>
     <?php } ?>
 </ul>
