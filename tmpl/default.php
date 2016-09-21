c6be3843a4e14da29e4cf0a7199e9e212a80bcdd
Giovanni Todaro <todarogiovanni1980@users.noreply.github.com>
2016-09-21 21:13:54 +0200

    Update default.php

--- tmpl/default.php
+++ tmpl/default.php
@@ -3,7 +3,16 @@
 <ul>
     <?php foreach ($items as $item) { ?>
     <li>
-        <?php echo $item->numero ." del ". $item->data ; ?>
+        <img
+			src="administrator/images/<?php echo ( $item->protocollo ) ? 'tick.png' : ( $item->protocollo != 1 ? 'publish_x.png' : 'disabled.png' );?>"
+			width="16" height="16" border="0"
+			alt="<?php echo ( $item->protocollo ) ? JText::_( "Protocollo numero " . $item->protocollo ) : JText::_( 'Non Protocollata' );?>" /> 
+		<img
+			src="administrator/images/<?php echo ( $item->filemissionefirmata ) ? 'edit_f2.png' : ( $item->filemissionefirmata != 1 ? 'edit.png' : 'edit.png' );?>"
+			width="16" height="16" border="0"
+			alt="<?php echo ( $item->protocollo ) ? JText::_( "Firmata " . $item->protocollo ) : JText::_( 'da firmare' );?>" />  
+			
+		<?php echo $item->numero ." del ". $item->data ; ?>
     </li>
     <?php } ?>
 </ul>
