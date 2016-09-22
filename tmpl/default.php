910e3da90385366d2af54aa4da154df12545866d
Giovanni Todaro <todarogiovanni1980@users.noreply.github.com>
2016-09-22 10:12:36 +0200

    Update default.php
    Aggiunto le icone di stato relative ai parametri di configurazione del modulo

--- tmpl/default.php
+++ tmpl/default.php
@@ -4,18 +4,18 @@
     <?php foreach ($items as $item) { ?>
     <li>
     	<img
-			src="administrator/images/<?php echo ( $item->rimborso ) ? 'tick.png' : ( $item->rimborso != 1 ? 'publish_x.png' : 'disabled.png' );?>"
-			width="16" height="16" border="0"
-			alt="<?php echo ( $item->rimborso ) ? JText::_( "Rimborsata " . $item->rimborso ) : JText::_( 'Non lavorata' );?>" /> 
+		src="administrator/images/<?php echo ( $item->rimborso ) ? 'tick.png' : ( $item->rimborso != 1 ? 'publish_x.png' : 'disabled.png' );?>"
+		width="16" height="16" border="0"
+		alt="<?php echo ( $item->rimborso ) ? JText::_( "Rimborsata " . $item->rimborso ) : JText::_( 'Non lavorata' );?>" /> 
         <img
-			src="administrator/images/<?php echo ( $item->protocollo ) ? 'tick.png' : ( $item->protocollo != 1 ? 'publish_x.png' : 'disabled.png' );?>"
-			width="16" height="16" border="0"
-			alt="<?php echo ( $item->protocollo ) ? JText::_( "Protocollo numero " . $item->protocollo ) : JText::_( 'Non Protocollata' );?>" /> 
+		src="administrator/images/<?php echo ( $item->protocollo ) ? 'tick.png' : ( $item->protocollo != 1 ? 'publish_x.png' : 'disabled.png' );?>"
+		width="16" height="16" border="0"
+		alt="<?php echo ( $item->protocollo ) ? JText::_( "Protocollo numero " . $item->protocollo ) : JText::_( 'Non Protocollata' );?>" /> 
 	<img
-			src="administrator/images/<?php echo ( $item->filemissionefirmata ) ? 'edit_f2.png' : ( $item->filemissionefirmata != 1 ? 'edit.png' : 'edit.png' );?>"
-			width="16" height="16" border="0"
-			alt="<?php echo ( $item->protocollo ) ? JText::_( "Firmata " . $item->protocollo ) : JText::_( 'da firmare' );?>" />  
-			
+		src="administrator/images/<?php echo ( $item->filemissionefirmata ) ? 'edit_f2.png' : ( $item->filemissionefirmata != 1 ? 'edit.png' : 'edit.png' );?>"
+		width="16" height="16" border="0"
+		alt="<?php echo ( $item->filemissionefirmata ) ? JText::_( "Firmata " . $item->filemissionefirmata ) : JText::_( 'da firmare' );?>" />
+	
 	<?php echo $item->numero ." del ". $item->data ; ?>
     </li>
     <?php } ?>
