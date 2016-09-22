2dde0c303b675b6cabc9f22ee00042e17b8e04d8
Giovanni Todaro <todarogiovanni1980@users.noreply.github.com>
2016-09-22 10:43:35 +0200

    Update mod_missioni.php
    Aggiunto i parametri di configurazione del modulo

--- mod_missioni.php
+++ mod_missioni.php
@@ -4,12 +4,16 @@ defined('_JEXEC') or die('Direct Access to this location is not allowed.');
 
 // get a parameter from the module's configuration
 $missionsCount = $params->get('missionscount');
+$firmare = $params->get('firmare');
+$rimborsare = $params->get('rimborsare');
+$protocollare = $params->get('protocollare');
+$data = $params->get('data');
 
 // include the helper file
 require_once(dirname(__FILE__).DS.'helper.php');
 
 // get the items to display from the helper
-$items = ModMissioniHelper::getItems($missionsCount);
+$items = ModMissioniHelper::getItems($missionsCount,$protocollare,$firmare,$rimborsare,$data);
 
 // include the template for display
 require(JModuleHelper::getLayoutPath('mod_missioni'));
